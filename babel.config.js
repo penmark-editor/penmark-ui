8f8725227eb094357b897a945e3cd7643a09525f
shobokshy <>
2019-08-17 11:37:06 +1000

    Fixes

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
 	api.cache(true);
 
-	const presets = [ "@babel/env", "@babel/react", "@babel/typescript" ];
+	const presets = [ "@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript" ];
 	const plugins = [ "@babel/proposal-class-properties", "@babel/proposal-object-rest-spread" ];
 
 	return {
